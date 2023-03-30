101dce0f2a1d456a5725faca38fdc6d9aeb2d6e9
Vishal Chauhan <vishalmessi550@gmail.com>
2023-03-31 03:35:53 +0530

    added style and touchups

--- main.js
+++ main.js
@@ -1,37 +1,53 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-
-//resize update
-const  sizes = {
+const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
 }
-//scene
+
 const scene = new THREE.Scene();
-//camera
-const camera = new THREE.PerspectiveCamera(70, sizes.width/sizes.height, 0.1, 100);
-camera.position.z = 10;
+const camera = new THREE.PerspectiveCamera(70, sizes.width/sizes.height, 0.1, 1000);
 
-//object
-//  geo and mat
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshBasicMaterial({
-    color: "#0083ff",
-});
-
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
-
-//renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(sizes.width, sizes.height);
-document.body.appendChild( renderer.domElement );
+renderer.setPixelRatio(2);
+document.body.appendChild(renderer.domElement);
 
-//render loop
-function animate() {
-    requestAnimationFrame( animate );
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+const geo = new THREE.SphereGeometry(3, 64, 64);
+const mat = new THREE.MeshStandardMaterial( {
+    color: 0x0083ff,
+} )
+const sphere = new THREE.Mesh(geo, mat);
+scene.add(sphere);
+
+camera.position.z = 20;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enableZoom = false;
+controls.enablePan = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    //update camera
+    camera.aspect = sizes.width/sizes.height;
+    camera.updateProjectionMatrix();
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height);
+})
+
+function loop() {
+    controls.update();
+    requestAnimationFrame(loop);
     renderer.render(scene, camera);
 }
 
-animate();
+loop();
